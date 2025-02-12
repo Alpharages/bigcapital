5b2c0b69263b8f273a6139921ab7b3d5ea20e8b5
khakanali <khakan.ali@alpharages.com>
2025-02-13 02:39:56 +0500

    fixed lint issue and add global types

--- packages/server/.eslintrc.js
+++ packages/server/.eslintrc.js
@@ -1,33 +1,43 @@
 module.exports = {
   env: {
-    browser: true,
-    es6: true,
+    node: true, // Enable Node.js global variables and scoping
+    es6: true,  // Enable ES6 features
   },
-  extends: ['airbnb-base', 'airbnb-typescript'],
-  parser: '@typescript-eslint/parser',
+  extends: [
+    'airbnb-base', // Airbnb's base ESLint rules
+    'airbnb-typescript/base', // Airbnb's TypeScript rules
+    'plugin:node/recommended', // Recommended rules for Node.js
+  ],
+  parser: '@typescript-eslint/parser', // Use the TypeScript parser
   parserOptions: {
-    ecmaVersion: 2018,
-    sourceType: 'module',
-    project: 'tsconfig.json',
-    tsconfigRootDir: './',
+    ecmaVersion: 2020, // Use the latest ECMAScript version
+    sourceType: 'module', // Use ES modules
+    project: './tsconfig.json', // Path to your tsconfig.json
   },
-  globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly',
-  },
-  plugins: ['import'],
+  plugins: [
+    '@typescript-eslint', // TypeScript plugin
+    'import', // Import plugin for resolving imports
+    'node', // Node.js plugin
+  ],
   rules: {
-    'import/no-unresolved': 'error',
-    'import/prefer-default-export': 'off',
+    'import/no-unresolved': 'error', // Ensure imports are resolved
+    'import/prefer-default-export': 'off', // Allow named exports
+    'node/no-missing-import': 'off', // Disable this rule as it conflicts with TypeScript's import resolution
+    'node/no-unsupported-features/es-syntax': 'off',
+    'node/no-unsupported-features/node-builtins': 'error',
+    'node/shebang': 'error',
+    'no-console': 'error',
+    'no-process-exit': 'off', // Allow the use of process.exit()
+    'no-underscore-dangle': 'warn',
   },
   settings: {
     'import/parsers': {
-      '@typescript-eslint/parser': ['.ts', '.tsx'],
+      '@typescript-eslint/parser': ['.ts', '.tsx'], // Use TypeScript parser for .ts and .tsx files
     },
     'import/resolver': {
       typescript: {
-        alwaysTryTypes: true,
-        project: 'tsconfig.json',
+        alwaysTryTypes: true, // Always try to resolve types under `<root>@types` directory
+        project: './tsconfig.json', // Path to your tsconfig.json
       },
     },
   },
---
+++ packages/server/src/types/globals.d.ts
@@ -0,0 +1,9 @@
+export {}; // This makes the file a module
+
+declare global {
+  var __root_dir: string;
+  var __resources_dir: string;
+  var __locales_dir: string;
+  var __views_dir: string;
+  var __storage_dir: string;
+}
